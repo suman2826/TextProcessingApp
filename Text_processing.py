fe89a73e3a93b2e4f5e4fea51211c5e359c50254
suman151 <sumangoel151@gmail.com>
2020-06-30 12:22:17 +0530

    Added Functionality

--- Text_processing.py
+++ Text_processing.py
@@ -3,3 +3,28 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import re
+def main():
+	st.title("Text Preprocessing")
+	raw_text = st.text_area("Enter Text Here")
+	c1 = "Convert to Lower Case"
+	c2 = "Remove Punctuation"
+	c3 = "Convert sentence into words"
+	c4 = "Remove Numbers"
+	c5 = "Remove Extra WhiteSpaces"
+	c6 = "Remove StopWords"
+	c7 = "Lemmatization(Converting to valid base word)"
+	choice2 = [c1,c2,c3,c4,c5,c6,c7]
+	choiceOperations = []
+	choiceOperations = st.multiselect("Operations",choice2)
+	print(choiceOperations)
+	out, flag = '',True
+	if st.button("Process"):
+		tokens = word_tokenize(raw_text)
+
+		if c1 in choiceOperations:
+			if flag:
+				out = raw_text.lower()
+				flag = False
+			else:
+				out = out.lower()
+		tokens1 = word_tokenize(out)
